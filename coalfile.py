0ff7458d8df6df5bf47bb806aac658a33a0c4b2c
Nik Voss <niklas.voss@gmail.com@>
2016-11-30 12:50:50 +0100

    Fix build issues.

--- coalfile.py
+++ coalfile.py
@@ -12,16 +12,15 @@ class ImguiFile(CoalFile):
             version = self.settings["version"]
             download(self.url % (version), self.zipfile)
             unzip(self.zipfile, 'temp')
+            cp('temp/imgui/', 'temp/') # TODO: mv would be cleaner
         else:
-            git_clone(self.repo, 'master', 'repo')
+            git_clone(self.repo, 'master', 'temp')
         if "patch" in self.settings:
-            with cd('repo/'):
+            with cd('temp/'):
                 patch(self.settings["patch"])
     def package(self):
-        cp('repo/*.h', 'include/')
-        cp('repo/*.cpp', 'src/')
-        cp('temp/imgui/*.h', 'include/')
-        cp('temp/imgui/*.cpp', 'src/')
+        cp('temp/*.h', 'include/')
+        cp('temp/*.cpp', 'src/')
     def info(self, generator):
         generator.add_include_dir('include/')
         generator.add_source_files('src/imgui.cpp', 'src/imgui_draw.cpp')
