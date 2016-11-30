f5d00b2c05d4d83d091b051ecf645db3b66d6f0f
Nik Voss <niklas.voss@gmail.com@>
2016-11-30 12:44:28 +0100

    Added possibility to get specific version.

--- coalfile.py
+++ coalfile.py
@@ -3,17 +3,25 @@ from util import cp, git_clone, cd, patch
 from os import path
 
 class ImguiFile(CoalFile):
-    url = "https://github.com/ocornut/imgui.git"
+    repo = "https://github.com/ocornut/imgui.git"
+    url = "https://github.com/ocornut/imgui/archive/v%s.zip"
+    zipfile = "imgui.zip"
     exports = ["include", "src"]
-
     def prepare(self):
-        git_clone(self.url, 'master', 'repo')
+        if "version" in self.settings:
+            version = self.settings["version"]
+            download(self.url % version, self.zipfile)
+            unzip(self.zipfile, 'temp')
+        else:
+            git_clone(self.repo, 'master', 'repo')
         if "patch" in self.settings:
             with cd('repo/'):
                 patch(self.settings["patch"])
     def package(self):
         cp('repo/*.h', 'include/')
         cp('repo/*.cpp', 'src/')
+        cp('temp/imgui/*.h', 'include/')
+        cp('temp/imgui/*.cpp', 'src/')
     def info(self, generator):
         generator.add_include_dir('include/')
         generator.add_source_files('src/imgui.cpp', 'src/imgui_draw.cpp')
